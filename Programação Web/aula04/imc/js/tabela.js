9bc8e35b6bf1c97d38a25b6f51694d0012b88ec6
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2023-09-12 21:18:45 -0300

    web

---
+++ Programação Web/aula04/imc/js/tabela.js
@@ -0,0 +1,41 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for(let i=0; i < pacientes.length; i++){
+    const paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhvalido = true;
+    var alturaEhValida = true;
+
+    if( peso <=0 || peso >= 300){
+        pesoEhvalido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (peso<=0 || altura >= 3){
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Invalida";
+        paciente.classList.add("paciente-invalido");
+
+    }
+
+    if (pesoEhvalido && alturaEhValida) {
+        const imcCalculado = calcularImc(peso, altura);
+
+        tdImc.textContent = imcCalculado;
+    }
+
+    function calcularImc(peso, altira){
+        var imc = 0;
+        imc = peso / (altura * altura);
+        return imc.toFixed(2);
+    }
+}
