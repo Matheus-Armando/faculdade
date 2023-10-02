ed52e242a7994bcc3f658f2ef62d59a1cea73b24
Matheus-Armando <matheuschioquetta1@gmail.com>
2023-10-02 08:46:59 -0300

    calc

---
+++ Programação Web/trab2/calculadora.js
@@ -0,0 +1,88 @@
+let valorAtual = '';
+let valorAnterior = '';
+let operador = '';
+
+function adicionarNumero(numero) {
+  valorAtual += numero;
+  atualizarResultado();
+}
+
+function adicionarOperador(op) {
+  if (valorAtual !== '') {
+    if (operador !== '') {
+      calcular();
+    }
+    operador = op;
+    valorAnterior = valorAtual;
+    valorAtual = '';
+    atualizarResultado();
+  }
+}
+
+function adicionarDecimal(decimal) {
+  if (valorAtual.indexOf('.') === -1) {
+    valorAtual += decimal;
+    atualizarResultado();
+  }
+}
+
+function calcular() {
+  if (valorAtual !== '' && valorAnterior !== '') {
+    const num1 = parseFloat(valorAnterior);
+    const num2 = parseFloat(valorAtual);
+
+    switch (operador) {
+      case '+':
+        valorAtual = String(num1 + num2);
+        break;
+      case '-':
+        valorAtual = String(num1 - num2);
+        break;
+      case '*':
+        valorAtual = String(num1 * num2);
+        break;
+      case '/':
+        if (num2 !== 0) {
+          valorAtual = String(num1 / num2);
+        } else {
+          valorAtual = 'Erro';
+        }
+        break;
+    }
+
+    operador = '';
+    valorAnterior = '';
+    atualizarResultado();
+  }
+}
+
+function limpar() {
+  valorAtual = '';
+  valorAnterior = '';
+  operador = '';
+  atualizarResultado();
+}
+
+function atualizarResultado() {
+  document.getElementById('resultado').value = valorAtual;
+}
+
+document.addEventListener('keydown', function(event) {
+    const teclaPressionada = event.key;
+    const teclaCode = event.code;
+  
+    if (/[0-9+\-*/.=]|Enter/.test(teclaPressionada)) {
+      if (teclaPressionada === '.') {
+        adicionarDecimal(teclaPressionada);
+      } else if (teclaPressionada === '=' || teclaPressionada === 'Enter') {
+        calcular();
+      } else if (/[+\-*/]/.test(teclaPressionada)) {
+        adicionarOperador(teclaPressionada);
+      } else if (teclaCode === 'KeyC' || teclaCode === 'Keyc') {
+        limpar();
+      } else {
+        adicionarNumero(teclaPressionada);
+      }
+    }
+  });
+ 
